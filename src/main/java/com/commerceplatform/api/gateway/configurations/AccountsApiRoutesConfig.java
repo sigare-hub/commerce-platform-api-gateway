142ea31ca57d78908a5825d83de39d11d81f846a
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-04-29 01:48:08 -0300

    application configurations

--- src/main/java/com/commerceplatform/api/gateway/configurations/AccountsApiRoutesConfig.java
+++ src/main/java/com/commerceplatform/api/gateway/configurations/AccountsApiRoutesConfig.java
@@ -2,6 +2,7 @@ package com.commerceplatform.api.gateway.configurations;
 
 import com.commerceplatform.api.gateway.security.filters.AuthenticationFilter;
 
+import com.commerceplatform.api.gateway.security.filters.CsrfHeaderFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -10,38 +11,39 @@ import org.springframework.http.HttpMethod;
 
 @Configuration
 public class AccountsApiRoutesConfig {
-    private final AuthenticationFilter authenticationFilter;
+    private static final String API_ACCOUNTS_URI = "http://localhost:4000/api";
 
-    public AccountsApiRoutesConfig(AuthenticationFilter authenticationFilter) {
-        this.authenticationFilter = authenticationFilter;
-    }
+    private AuthenticationFilter authenticationFilter;
+
+    public AccountsApiRoutesConfig(CsrfHeaderFilter csrfHeaderFilter) {}
 
     @Bean
-    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+    public RouteLocator routeLocator(RouteLocatorBuilder builder, CsrfHeaderFilter csrfHeaderFilter) {
         return builder.routes()
             .route("login", r -> r.path("/auth/login")
-                .filters(f -> f.filter(authenticationFilter))
-                .uri("http://localhost:4000/api"))
+                .filters(f -> f.filter(csrfHeaderFilter).filter(authenticationFilter))
+                .uri(API_ACCOUNTS_URI))
             .route("recovery-password", r -> r.path("/recovery-password/**")
-                .filters(f -> f.filter(authenticationFilter))
-                .uri("http://localhost:4000/api"))
+                .filters(f -> f.filter(csrfHeaderFilter).filter(authenticationFilter))
+                .uri(API_ACCOUNTS_URI))
             .route("create-user", r -> r.path("/user")
-                .filters(f -> f.filter(authenticationFilter))
-                .uri("http://localhost:4000/api"))
+//            .filters(f -> f.filter(authenticationFilter))
+                .uri(API_ACCOUNTS_URI))
             .route("create-role", r -> r.method(HttpMethod.POST).and().path("/role")
-                .filters(f -> f.filter(authenticationFilter))
-                .uri("http://localhost:4000/api"))
+                .filters(f -> f.filter(csrfHeaderFilter).filter(authenticationFilter))
+                .uri(API_ACCOUNTS_URI))
             .route("get-roles", r -> r.method(HttpMethod.GET).and().path("/role")
-                .filters(f -> f.filter(authenticationFilter))
-                .uri("http://localhost:4000/api"))
+                .filters(f -> f.filter(csrfHeaderFilter))
+                .uri(API_ACCOUNTS_URI))
             .route("get-users", r -> r.method(HttpMethod.GET).and().path("/user")
-                .filters(f -> f.filter(authenticationFilter))
-                .uri("http://localhost:4000/api"))
+                .filters(f -> f.filter(csrfHeaderFilter).filter(authenticationFilter))
+                .uri(API_ACCOUNTS_URI))
             .route("get-user-types", r -> r.path("/user-type")
-                .uri("http://localhost:4000/api"))
+                .uri(API_ACCOUNTS_URI))
             .route("update-user-roles", r -> r.method(HttpMethod.PATCH).and().path("/users-roles")
-                .filters(f -> f.filter(authenticationFilter))
-                .uri("http://localhost:4000/api"))
+                .filters(f -> f.filter(csrfHeaderFilter).filter(authenticationFilter))
+                .uri(API_ACCOUNTS_URI))
             .build();
     }
+
 }
